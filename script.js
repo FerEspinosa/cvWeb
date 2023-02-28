2b1f9fd64cf5823d5b5a95a8697e4b83acaefbbc
FerEspinosa <67436442+FerEspinosa@users.noreply.github.com>
2023-02-28 05:18:26 -0300

    first commit
    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+
+for (const navLink of navLinks) {
+	navLink.addEventListener('click', function(e) {
+		e.preventDefault();
+		const sectionId = this.getAttribute('href');
+		const section = document.querySelector(sectionId);
+		const sectionTop = section.offsetTop;
+		window.scrollTo({ top: sectionTop, behavior: 'smooth' });
+	});
+}

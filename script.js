898af6cefeba9d8ec58489045a74563fd04e4fce
Fernando Espinosa <67436442+FerEspinosa@users.noreply.github.com>
2024-09-21 19:53:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const navLinks = document.querySelectorAll('nav ul li a');
 
 for (const navLink of navLinks) {
 	navLink.addEventListener('click', function(e) {
-		<! -- e.preventDefault(); -->
-		const sectionId = this.getAttribute('href');
-		const section = document.querySelector(sectionId);
-		const sectionTop = section.offsetTop;
-		window.scrollTo({ top: sectionTop, behavior: 'smooth' });
+		const hrefValue = this.getAttribute('href');
+
+		// Si el enlace es un ancla que apunta a una sección interna
+		if (hrefValue.startsWith('#')) {
+			e.preventDefault();  // Evita la acción predeterminada solo para anclas internas
+			const section = document.querySelector(hrefValue);
+			if (section) {
+				const sectionTop = section.offsetTop;
+				window.scrollTo({ top: sectionTop, behavior: 'smooth' });
+			}
+		} 
+		// Si es un enlace externo (como un cambio de página), el comportamiento por defecto es permitido
 	});
 }

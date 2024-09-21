64c7ec974864c4b2c0d1cc1c6a7a165d240d752d
Fernando Espinosa <67436442+FerEspinosa@users.noreply.github.com>
2024-09-21 11:13:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const navLinks = document.querySelectorAll('nav ul li a');
 
 for (const navLink of navLinks) {
 	navLink.addEventListener('click', function(e) {
-		e.preventDefault();
+		<! -- e.preventDefault(); -->
 		const sectionId = this.getAttribute('href');
 		const section = document.querySelector(sectionId);
 		const sectionTop = section.offsetTop;
